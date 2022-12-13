4bc1f96630c55cc74f0bd7e9171017843c5eeb04
Abhinav-676 <harshikeshabhinav670@gmail.com>
2022-12-13 20:41:51 +0530

    Header 1.1

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 function displayMenu(){
 
     let classList = document.getElementsByClassName("drop-down-menu")[0].classList
+    let blankCover = document.getElementsByClassName("blank-cover")[0].classList
     let list = Array.from(classList)
 
     let checker = list.filter(function (a) {
@@ -15,8 +16,11 @@ function displayMenu(){
     if (checker != "drop-down-menu-display")
     {
         classList.add("drop-down-menu-display")
+        blankCover.add("blank-cover-on")
+
     }
     else{
         classList.remove("drop-down-menu-display")
+        blankCover.remove("blank-cover-on")
     }
 } 
